f5e4c04aad1bb6a66f28376ad0708e212e902aae
赵越 <shadowingszy@outlook.com>
2020-01-28 10:18:05 +0800

    修正了代码

--- source/index.tsx
+++ source/index.tsx
@@ -1,12 +1,4 @@
 import { documentReady, render, createCell } from 'web-cell';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { MapViz } from "./page/MapViz";
-=======
 import { MapViz } from './page/MapViz';
->>>>>>> 新增疫情地图通用组件
-=======
-import { MapViz } from './page/MapViz';
->>>>>>> c17cc134afb9b6a7d0e500fd5ae0b4d9d5e6074d
 
 documentReady.then(() => render(<MapViz />));
--- source/page/MapViz.tsx
+++ source/page/MapViz.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { observer } from 'mobx-web-cell';
-import { component, mixin, createCell } from 'web-cell';
-
-interface MapVizState {
-    data?: any;
-    filterFunc?: () => void;
-=======
-=======
->>>>>>> c17cc134afb9b6a7d0e500fd5ae0b4d9d5e6074d
 /**
  * WebCell疫情地图展示页面
  * 使用VirusMap组件构建疫情地图的示例，包含了国家级、省级不同粒度疫情地图的查看
@@ -22,26 +11,10 @@ import mockData from "../../mock/map_viz_mock_data.js";
 
 interface State {
   index: number;
-<<<<<<< HEAD
->>>>>>> 新增疫情地图通用组件
-=======
->>>>>>> c17cc134afb9b6a7d0e500fd5ae0b4d9d5e6074d
 }
 
 @observer
 @component({
-<<<<<<< HEAD
-<<<<<<< HEAD
-    tagName: 'map-viz',
-    renderTarget: 'children'
-})
-export class MapViz extends mixin<{}, MapVizState>() {
-    public render(_, input: MapVizState) {
-        return <h1>Hello World!!</h1>
-    } 
-=======
-=======
->>>>>>> c17cc134afb9b6a7d0e500fd5ae0b4d9d5e6074d
   tagName: 'map-viz',
   renderTarget: 'children'
 })
@@ -73,8 +46,4 @@ export class MapViz extends mixin<{}, State>() {
 
     );
   }
-<<<<<<< HEAD
->>>>>>> 新增疫情地图通用组件
-=======
->>>>>>> c17cc134afb9b6a7d0e500fd5ae0b4d9d5e6074d
 }
