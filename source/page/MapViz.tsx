8fbf5193e31da56bcc922da939933abc76794a9d
赵越 <shadowingszy@outlook.com>
2020-02-02 00:01:48 +0800

    添加router

--- source/page/MapViz.tsx
+++ source/page/MapViz.tsx
@@ -6,67 +6,38 @@
  */
 
 import { observer } from 'mobx-web-cell';
-import { component, mixin, createCell } from 'web-cell';
-import { HierarchicalVirusMap } from '../components/HierarchicalVirusMap';
+import { component, createCell } from 'web-cell';
+import { HierarchicalVirusMapDemo } from './HierarchicalVirusMapDemo';
+import { InformationMapDemo } from './InformationMapDemo';
+import { History, HTMLRouter } from 'cell-router/source';
 
-// import rawData from '../../data/isaaclin/current.json';
-// import { convertCountry } from '../adapters/isaaclin';
-import patchData from '../../data/isaaclin/patch.json';
-import {
-  convertProvincesSeries,
-  convertCountrySeries,
-} from '../adapters/isaaclin';
-
-import { InformationMap, GeoData, Metadata } from '../components/InformationMap';
-import informationMockData from '../../mock/information_map_general_mock_data';
-import rawData from '../../data/isaaclin/history.json';
-import overviewData from '../../data/isaaclin/overall.json';
-
-interface State {
-  path: string[];
-}
-
-const resolution = 3600000 * 24;
-const data = {
-  provincesSeries: convertProvincesSeries(
-    [...rawData['results'], ...patchData],
-    resolution,
-    true
-  ),
-  countrySeries: convertCountrySeries(overviewData['results'], resolution)
-};
+const history = new History();
 
 @observer
 @component({
   tagName: 'map-viz',
   renderTarget: 'children'
 })
-export class MapViz extends mixin<{}, State>() {
-  state = { path: [] };
-  chartOnClickCallBack(params) {
-    if (params.name) {
-      this.setState({ path: [...this.state.path, params.name] });
-    }
-  }
+export class MapViz extends HTMLRouter {
+  protected history = history;
+  protected routes = [
+    { paths: ['virusMap'], component: HierarchicalVirusMapDemo },
+    { paths: ['informationMap'], component: InformationMapDemo }
+  ];
 
-  public generateMockData(): GeoData[] {
-
-    return [];
-  }
-
-  public render() {
+  public render({}, {}) {
     return (
-      <div>
-        <div style={{ width: '100%', height: '100%' }}>
-          <HierarchicalVirusMap data={data} resolution={resolution} />
-        </div>
-        <div style={{ width: '100%', height: '80%' }}>
-          <InformationMap
-            initPoint={informationMockData.initPoint}
-            zoom={informationMockData.zoom}
-            markers={informationMockData.makerArray} />
-        </div>
-      </div>
+      <main>
+        <ul>
+          <li>
+            <a href="virusMap">疫情地图组件</a>
+          </li>
+          <li>
+            <a href="informationMap">通用地图组件</a>
+          </li>
+        </ul>
+        <div>{super.render({}, {})}</div>
+      </main>
     );
   }
 }
