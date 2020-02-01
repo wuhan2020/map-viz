58dd35d5beba96b71788dc51a0f03bd3b5543474
Ritaotao <ritaotao28@gmail.com>
2020-01-31 20:56:33 -0500

    virusChart add legend, color, axis title, issue#73

--- source/components/VirusChart.tsx
+++ source/components/VirusChart.tsx
@@ -135,13 +135,20 @@ export class VirusChart extends mixin<Props, State>() {
       title: {
         text: area + '疫情确诊/疑似数'
       },
+      legend: {
+        orient: 'horizontal',
+        data: ['确诊', '疑似']
+      },
       tooltip: {
         trigger: 'axis'
       },
       xAxis: {
+        name: '时间',
         type: 'time'
       },
-      yAxis: {},
+      yAxis: {
+        name: '例'
+      },
       series: [
         {
           name: '确诊',
@@ -157,7 +164,8 @@ export class VirusChart extends mixin<Props, State>() {
           stack: '总量',
           areaStyle: {}
         }
-      ]
+      ],
+      color: ['#FF2400', '#FFC30F']
     };
   }
 
@@ -180,9 +188,16 @@ export class VirusChart extends mixin<Props, State>() {
         trigger: 'axis'
       },
       xAxis: {
+        name: '时间',
         type: 'time'
       },
-      yAxis: {},
+      yAxis: {
+        name: '例'
+      },
+      legend: {
+        orient: 'horizontal',
+        data: ['治愈', '死亡']
+      },
       series: [
         {
           name: '治愈',
@@ -194,7 +209,8 @@ export class VirusChart extends mixin<Props, State>() {
           data: deadData,
           type: 'line'
         }
-      ]
+      ],
+      color: ['#30cb00', '#808080']
     };
   }
 
