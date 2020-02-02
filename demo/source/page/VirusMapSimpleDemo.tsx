553f9ef66a7f0adca8e2e460957437249acd25a2
yarray <08to09@gmail.com>
2020-02-02 21:23:40 +0800

    demo 自动设定色彩间断点

--- demo/source/page/VirusMapSimpleDemo.tsx
+++ demo/source/page/VirusMapSimpleDemo.tsx
@@ -9,6 +9,21 @@ import { observer } from 'mobx-web-cell';
 import { component, mixin, createCell } from 'web-cell';
 import { VirusData, VirusMap } from 'wuhan2020-map-viz';
 
+function autoBreaks(values: number[]) {
+  const base = [1, 10, 50, 100, 500, 1000];
+  const k =
+    (Math.floor(Math.max(...values.filter(v => v !== undefined)) / 5 / 500) *
+      500) /
+    Math.max(...base);
+  let res = base.map(b => k * b);
+  res[0] = 1;
+  return res;
+}
+
+const breaks = autoBreaks(
+  Object.values(VirusData.countryData.provinces).map(prov => prov.confirmed)
+);
+
 @observer
 @component({
   tagName: 'virus-map-simple-demo',
@@ -21,6 +36,7 @@ export class VirusMapSimpleDemo extends mixin<{}, {}>() {
         <VirusMap
           name="中国"
           data={VirusData.countryData.provinces}
+          breaks={breaks}
           chartData={VirusData}
           currentChartArea="中国"
         ></VirusMap>
