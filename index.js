fa0236fbbf13874140fdf637ad0a4f83d1ac6059
赵越 <shadowingszy@outlook.com>
2020-02-03 17:28:26 +0800

    调整代码结构

---
+++ index.js
@@ -0,0 +1,7 @@
+import { HierarchicalVirusMap } from './src/components/virusMap/hierarchicalVirusMap';
+import { InformationMap } from './src/components/informationMap/informationMap';
+
+module.exports = {
+  HierarchicalVirusMap,
+  InformationMap
+}
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import { HierarchicalVirusMap } from './virusMap/hierarchicalVirusMap';
-import { InformationMap } from './informationMap/informationMap';
-
-export {
-  HierarchicalVirusMap,
-  InformationMap
-}
