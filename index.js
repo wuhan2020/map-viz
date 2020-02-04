854a5772883842efed2c6a7863e6bbd2aae3f01d
赵越 <shadowingszy@outlook.com>
2020-02-04 16:03:43 +0800

    删去不必要的文件

--- index.js
+++
@@ -1,7 +0,0 @@
-import { HierarchicalVirusMap } from './src/components/virusMap/hierarchicalVirusMap';
-import { InformationMap } from './src/components/informationMap/informationMap';
-
-module.exports = {
-  HierarchicalVirusMap,
-  InformationMap
-}
